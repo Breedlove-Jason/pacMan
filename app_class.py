f55249f0519764d11232ef5b4a14da442c9e63c6
Jason Breedlove <j@jasonbreedlove.com>
2021-11-25 22:33:14 -0800

    finished the home screen text, the second state, and created the skeleton of the playing functions

--- app_class.py
+++ app_class.py
@@ -20,17 +20,25 @@ class App:
                 self.start_events()
                 self.start_update()
                 self.start_draw()
+            elif self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
+            else:
+                self.running = False
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
 
     '''####################### HELPER FUNCTIONS #########################'''
-    def draw_text(self, words, screen, pos, size, color, font_name):
+
+    def draw_text(self, words, screen, pos, size, color, font_name, centered=False):
         font = pygame.font.SysFont(font_name, size)
         text = font.render(words, False, color)
         text_size = text.get_size()
-        pos[0] = pos[0]-text_size[0]//2
-        pos[1] = pos[1]-text_size[1]//2
+        if centered:
+            pos[0] = pos[0] - text_size[0] // 2
+            pos[1] = pos[1] - text_size[1] // 2
         screen.blit(text, pos)
 
     '''####################### INTRO FUNCTIONS ##########################'''
@@ -39,7 +47,7 @@ class App:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-            if event.type == pygame.KEYDOWN and event.key == K_SPACE:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 self.state = 'playing'
 
     def start_update(self):
@@ -47,5 +55,24 @@ class App:
 
     def start_draw(self):
         self.screen.fill(BLACK)
-        self.draw_text('PUSH SPACE BAR', self.screen, [WIDTH//2, HEIGHT//2], START_TEXT_SIZE, (255, 142, 15), START_FONT)
+        self.draw_text('PUSH SPACE BAR', self.screen, [WIDTH // 2, HEIGHT // 2], START_TEXT_SIZE, (255, 142, 15),
+                       START_FONT, centered=True)
+        self.draw_text('1 PLAYER ONLY', self.screen, [WIDTH // 2, HEIGHT // 2 + 75], START_TEXT_SIZE, (50, 201, 255),
+                       START_FONT, centered=True)
+        self.draw_text('HIGH SCORE', self.screen, [5, 1], START_TEXT_SIZE, (255, 255, 255),
+                       START_FONT)
+        pygame.display.update()
+
+    '''####################### PLAYING FUNCTIONS ##########################'''
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def playing_update(self):
+        pass
+
+    def playing_draw(self):
+        self.screen.fill(RED)
         pygame.display.update()
--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ FPS = 60
 
 # color settings
 BLACK = (0, 0, 0)
-ORANGE = (255, 142, 15)
+RED = (208, 22, 22)
 # font settings
-START_TEXT_SIZE = 22
+START_TEXT_SIZE = 26
 START_FONT = 'arial black'
 
 # player settings
