88a4457368cf661da7e9cbd67881d6fea30622ae
Jason Breedlove <j@jasonbreedlove.com>
2021-11-25 21:21:16 -0800

    created draw_text function and centered game text on the screen

--- app_class.py
+++ app_class.py
@@ -11,19 +11,41 @@ class App:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.intro = 'intro'
+        self.state = 'start'
 
     # run loop
     def run(self):
         while self.running:
-            if self.state == 'intro':
-                self.intro_events()
-                self.intro_update()
-                self.intro_draw()
+            if self.state == 'start':
+                self.start_events()
+                self.start_update()
+                self.start_draw()
+            self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
 
-    def intro_events(self):
+    '''####################### HELPER FUNCTIONS #########################'''
+    def draw_text(self, words, screen, pos, size, color, font_name):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(words, False, color)
+        text_size = text.get_size()
+        pos[0] = pos[0]-text_size[0]//2
+        pos[1] = pos[1]-text_size[1]//2
+        screen.blit(text, pos)
+
+    '''####################### INTRO FUNCTIONS ##########################'''
+
+    def start_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.KEYDOWN and event.key == K_SPACE:
+                self.state = 'playing'
+
+    def start_update(self):
+        pass
+
+    def start_draw(self):
+        self.screen.fill(BLACK)
+        self.draw_text('PUSH SPACE BAR', self.screen, [WIDTH//2, HEIGHT//2], START_TEXT_SIZE, (255, 142, 15), START_FONT)
+        pygame.display.update()
--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
 # screen settings
-WIDTH, HEIGHT = 500, 700
+WIDTH, HEIGHT = 448, 596
+FPS = 60
 
 # color settings
+BLACK = (0, 0, 0)
+ORANGE = (255, 142, 15)
+# font settings
+START_TEXT_SIZE = 22
+START_FONT = 'arial black'
+
 # player settings
+
 # bad guy settings
