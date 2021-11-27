12df7607a1397c12bef12d5dbe5e97b3801e5c56
Jason Breedlove <j@jasonbreedlove.com>
2021-11-27 15:00:52 -0800

    locked to grid

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+;
+settings.py,8/8/888c15d72e41c9f0f1882f4aea4c2d19f1a044eb
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+?
+player_class.py,0/7/07d445eef6febc3d04af622c21c83a4203d5014d
+<
+app_class.py,d/e/de9d1b24be99b8c966bd4cb9117e294798cf9ec3
--- player_class.py
+++ player_class.py
@@ -10,10 +10,20 @@ class Player:
         self.grid_pos = pos
         self.pix_pos = self.get_pix_pos()
         self.direction = vec(1, 0)
+        self.stored_direction = None
 
     # setting grid position in reference to pixel position
     def update(self):
         self.pix_pos += self.direction
+        if int(self.pix_pos.x + TOP_BOTTOM_BUFFER // 2) % self.app.cell_width == 0:
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+                if self.stored_direction is not None:
+                    self.direction = self.stored_direction
+        if int(self.pix_pos.y + TOP_BOTTOM_BUFFER // 2) % self.app.cell_height == 0:
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+                if self.stored_direction is not None:
+                    self.direction = self.stored_direction
+
         self.grid_pos[0] = (self.pix_pos[0] - TOP_BOTTOM_BUFFER + self.app.cell_width // 2) // self.app.cell_width + 1
         self.grid_pos[1] = (self.pix_pos[1] - TOP_BOTTOM_BUFFER + self.app.cell_height // 2) // self.app.cell_width + 1
 
@@ -22,11 +32,12 @@ class Player:
                            self.app.cell_width // 2 - 2)
         # draw rectangle to track grid position
         pygame.draw.rect(self.app.screen, RED, (self.grid_pos[0] * self.app.cell_width + TOP_BOTTOM_BUFFER // 2,
-                         self.grid_pos[1] * self.app.cell_height + TOP_BOTTOM_BUFFER // 2, self.app.cell_width,
-                         self.app.cell_height), 1)
+                                                self.grid_pos[1] * self.app.cell_height + TOP_BOTTOM_BUFFER // 2,
+                                                self.app.cell_width,
+                                                self.app.cell_height), 1)
 
     def move(self, direction):
-        self.direction = direction
+        self.stored_direction = direction
 
     def get_pix_pos(self):
         return \
