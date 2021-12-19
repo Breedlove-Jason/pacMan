424af34c012cbc3b146127f6e53900ad02a760a3
anonymous164 <j@jasonbreedlove.com>
2021-12-19 13:27:39 -0800

    created a method to increase player speed and a method to color the enemies

--- enemy_class.py
+++ enemy_class.py
@@ -5,19 +5,35 @@ vec = pygame.math.Vector2
 
 
 class Enemy:
-    def __init__(self, app, pos):
+    def __init__(self, app, pos, number):
         self.app = app
         self.grid_pos = pos
         self.pix_pos = self.get_pix_pos()
+        self.radius = int(self.app.cell_width // 2.3)
+        self.number = number
+        self.color = self.set_color()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pygame.draw.circle(self.app.screen, self.color, (int(self.pix_pos.x), int(self.pix_pos.y)), self.radius)
 
     def get_pix_pos(self):
         return vec((
                            self.grid_pos.x * self.app.cell_width) + TOP_BOTTOM_BUFFER // 2 + self.app.cell_width // 2,
                    (self.grid_pos.y * self.app.cell_height) + TOP_BOTTOM_BUFFER // 2 + self.app.cell_height // 2)
 
-    def update(self):
-        pass
-
-    def draw(self):
-        pygame.draw.circle(self.app.screen, (255, 255, 255), (int(self.pix_pos.x), int(self.pix_pos.y)),
-                           self.app.cell_width // 2)
+    def set_color(self):
+        if self.number == 0:
+            # blue
+            return 61, 51, 255
+        if self.number == 1:
+            # red
+            return 224, 34, 6
+        if self.number == 2:
+            # yellow
+            return 217, 244, 8
+        if self.number == 3:
+            # green
+            return 67, 255, 51
